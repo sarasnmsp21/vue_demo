5dd48b1988a127e442a73da287a8dbbbdad08c48
sarasnmsp21 <saraswati.gauda@wohlig.com>
2019-03-25 17:51:36 +0530

    footer uploaded

---
+++ src/views/saruufooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <custom-footer></custom-footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import customFooter from "@/components/sarasfooter.vue";
+
+export default {
+  name: "footer",
+  components: {
+    customFooter
+  }
+};
+</script>
