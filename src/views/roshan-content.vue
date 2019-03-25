becee20cb0ecfcb332317357deb9b82e583fc43e
prajaktakamble <prajakta.kamble@wohlig.com>
2019-03-25 18:05:09 +0530

    roshan content start

---
+++ src/views/roshan-content.vue
@@ -0,0 +1,164 @@
+<template>
+    <div>
+    
+    
+        <div class="background  img-fluid py-5">
+            <b-container>
+                <b-row class="justify-content-center">
+                    <b-col cols=8>
+                        <div class="text-center text-white">
+                            <div class="pt-3">
+                                <p class="pt-5">BY SIMPLEQADE</p>
+                            </div>
+                            <div class="pt-3">
+                                <p class="display-4 d-block text-waight-bold">
+                                    Landing pages occasion
+                                </p>
+                            </div>
+                            <p class="pt-3">Incline is set of landing and support pages aimed at helping companies promote new products and business launches.</p>
+                            <div class="pt-3">
+                                <button class="btn btn-outline-warning text-white">
+                                                                                <span class="small">PURCHASE NOW</span>
+                                                                              </button>
+                            </div>
+                        </div>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
+        <!-------------------- ---------------------- -->
+        <b-container>
+            <b-row class="justify-content-center">
+                <b-col cols=6>
+                    <div class="text-center mt-5">
+                        <h1 class="pt-5">Landing page variants</h1>
+                        <p class="pt-3 text-secondary">
+                            Incline comes with several professionally designed landing pages that can be easily adapted for any project.
+                        </p>
+                    </div>
+    
+                </b-col>
+            </b-row>
+            <b-row class="mx-auto my-5 ">
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4>Product landing </h4>
+                            <b-card-text>
+                                <p class="text-secondary">You can use this page to promote your product or a store.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+    
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4> Real estate landing
+                                <span class="bg-danger p-1"> new!</span>
+                            </h4>
+                            <b-card-text>
+                                <p class="text-secondary">You can use this page to promote your real estate.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+    
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4> Restaurant landing </h4>
+                            <b-card-text>
+                                <p class="text-secondary"> You can use this page to promote your restaurant.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+            </b-row>
+            <!-- ------------------------- -->
+            <b-row class="mx-auto my-5 ">
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4>App landing</h4>
+                            <b-card-text>
+                                <p class="text-secondary">You can use this page to promote your restaurant.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+    
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4>Company landing </h4>
+                            <b-card-text>
+                                <p class="text-secondary"> You can use this page to promote your restaurant.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+    
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4>
+                                Fullpage scrolling
+                                <span class="p-1 bg-danger"> New!</span>
+                            </h4>
+                            <b-card-text>
+                                <p class="text-secondary"> You can use this page to promote your restaurant.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+            </b-row>
+                   <!-- ------------------------- -->
+    
+    <b-row class="mx-auto my-5 ">
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4>App landing</h4>
+                            <b-card-text>
+                                <p class="text-secondary">You can use this page to promote your restaurant.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+    
+                <b-col md=4>
+                    <div>
+                        <b-card title="" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" class="mb-2 border-0 text-left">
+                            <h4>Company landing </h4>
+                            <b-card-text>
+                                <p class="text-secondary"> You can use this page to promote your restaurant.</p>
+                            </b-card-text>
+    
+                        </b-card>
+                    </div>
+                </b-col>
+                </b-row>
+        </b-container>
+    
+    
+    
+    </div>
+</template>
+
+<style lang="scss">
+    .background {
+        background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../assets/background-img.jpg");
+        background-size: cover;
+        width: 100%;
+        height: 600px;
+        border-radius: 0px !important;
+    }
+</style>
