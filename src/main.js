2e42b172368ef0aa58ae2001767959d9fbe063e2
Wohlig <ram.chaudhari@wohlig.com>
2019-03-27 12:38:44 +0530

    added fontawesome package to main js

--- src/main.js
+++ src/main.js
@@ -6,12 +6,19 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons' //fas fa
+
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons' //fab
+import {  } from '@fortawesome/free-regular-svg-icons' //far
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
  
-library.add(faCoffee)
- 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faCoffee) //fas
+library.add() //far
+library.add(faLinkedinIn) //fab
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
 
